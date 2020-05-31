c1aeb6d4a49a9561f75168975e87d3e52afc5995
deepa <deepa@192.168.0.14>
2020-05-31 05:48:45 -0500

    example

---
+++ src/com/test/rest/dummy/Emp2.java
@@ -0,0 +1,25 @@
+package com.test.rest.dummy;
+
+public class Emp2 {
+	private String fname;
+	private String lname;
+	
+	public Emp2(String fname, String lname) {
+		super();
+		this.fname = fname;
+		this.lname = lname;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	
+}
