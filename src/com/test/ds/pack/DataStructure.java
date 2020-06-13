22ec2a2beca931dbe1be4717dcab0103c200aaaf
beher <beher@192.168.0.30>
2020-06-13 12:54:39 -0500

    ds change

--- src/com/test/ds/pack/DataStructure.java
+++ src/com/test/ds/pack/DataStructure.java
@@ -4,6 +4,7 @@ import java.util.stream.IntStream;
 
 public class DataStructure {
 	public static void main(String[] args) {
+		//System
 		IntStream.rangeClosed(1, 100).forEach(System.out::println);
 	}
 	
