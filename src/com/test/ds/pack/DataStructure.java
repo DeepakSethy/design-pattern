cc1953669923e8585c54740a0df2d3476517516c
beher <beher@192.168.0.30>
2020-06-13 12:53:36 -0500

    ds file

---
+++ src/com/test/ds/pack/DataStructure.java
@@ -0,0 +1,10 @@
+package com.test.ds.pack;
+
+import java.util.stream.IntStream;
+
+public class DataStructure {
+	public static void main(String[] args) {
+		IntStream.rangeClosed(1, 100).forEach(System.out::println);
+	}
+	
+}
